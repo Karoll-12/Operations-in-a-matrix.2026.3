0d4cc86d1fba2898844cacaec4149d4ed0dde8a0
KAROLL STEFANNY MOORE LOPEZ <karollmoore1141863@correo.itm.edu.co>
2026-03-08 21:12:08 -0500

    Operations-in-a-matrix.2026.3

---
+++ MatrixProgram/MatrixProgram/Bckend/SquareMatrix.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace MatrizOO
+{
+    public class SquareMatrix
+    {
+        private int[,] matrix;
+        private int n;
+
+        public SquareMatrix(int n)
+        {
+            this.n = n;
+            matrix = new int[n, n];
+        }
+
+        public void FillMatrix()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = (i + 1) - j;
+                }
+            }
+        }
+
+        public void ShowMatrix()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i, j],4}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int SumMatrix()
+        {
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+
+            return sum;
+        }
+
+        public int MaxValue()
+        {
+            int max = matrix[0, 0];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] > max)
+                        max = matrix[i, j];
+                }
+            }
+
+            return max;
+        }
+
+        public int MinValue()
+        {
+            int min = matrix[0, 0];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] < min)
+                        min = matrix[i, j];
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ MatrixProgram/MatrixProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MatrizOO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese orden de la matriz: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        SquareMatrix matrix = new SquareMatrix(n);
+
+        matrix.FillMatrix();
+
+        Console.WriteLine("\nMatriz generada:\n");
+        matrix.ShowMatrix();
+
+        Console.WriteLine("\nLa sumatoria es: " + matrix.SumMatrix());
+        Console.WriteLine("El valor máximo es: " + matrix.MaxValue());
+        Console.WriteLine("El valor mínimo es: " + matrix.MinValue());
+    }
+}
